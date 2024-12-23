fdd0d19768f6882c3fcb3069490b449d7f3a6f99
Anubhav Jain <anubhav27nov@gmail.com>
2024-12-23 15:54:45 +0530

    Added Features

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Hero from './components/Hero.jsx';
 import About from './components/About.jsx'; 
 import Navbar from './components/Navbar.jsx';
-
+import Features from './components/Features.jsx';
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
       <About />
+      <Features />
     </div>
   );
 };
