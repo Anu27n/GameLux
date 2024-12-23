479110f0df40916a5b9b5abd6ecea62d0538fc45
Anubhav Jain <anubhav27nov@gmail.com>
2024-12-23 16:36:40 +0530

    Added Contact and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import About from './components/About.jsx';
 import Navbar from './components/Navbar.jsx';
 import Features from './components/Features.jsx';
 import Story from './components/Story.jsx';
+import Contact from './components/Contact.jsx';
+import Footer from './components/Footer.jsx';
 const App = () => {
   return (
     <div>
@@ -11,6 +13,8 @@ const App = () => {
       <About />
       <Features />
       <Story/>
+      <Contact />
+      <Footer />
     </div>
   );
 };
