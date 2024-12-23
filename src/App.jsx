ad3c5c1fe06079144702c47187f0e7fa7ab11904
Anubhav Jain <anubhav27nov@gmail.com>
2024-12-23 13:32:36 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from './components/Hero.jsx';
-import About from './components/About.jsx'; // Ensure you have this component
+import About from './components/About.jsx'; 
+import Navbar from './components/Navbar.jsx';
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Hero />
       <About />
     </div>
